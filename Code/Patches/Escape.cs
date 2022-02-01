4d30cb2b7bc3d14b51e89e14f9340ddbdf43a3fd
algernon-A <aeneas@aloku.net>
2022-02-02 02:23:58 +1100

    Add escape key handling.

---
+++ Code/Patches/Escape.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+
+
+namespace TransferController
+{
+    /// <summary>
+    /// Harmony patch to implement escape key handling.
+    /// </summary>
+    [HarmonyPatch(typeof(GameKeyShortcuts), "Escape")]
+    public static class EscapePatch
+    {
+        /// <summary>
+        /// Harmony prefix patch to cancel the zoning tool when it's active and the escape key is pressed.
+        /// </summary>
+        /// <returns>True (continue on to game method) if the zoning tool isn't already active, false (pre-empt game method) otherwise</returns>
+        public static bool Prefix()
+        {
+            // Is the zoning tool active?
+            if (TCTool.IsActiveTool)
+            {
+                // Yes; toggle tool status and return false (pre-empt original method).
+                TCTool.ToggleTool();
+                return false;
+            }
+
+            // Tool not active - don't do anything, just go on to game code.
+            return true;
+        }
+    }
+}
