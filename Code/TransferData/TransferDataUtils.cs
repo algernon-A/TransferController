7d76b7359ebf9278aec1caf22e7b446211a0f307
Tal <talrofeh@gmail.com>
2022-10-02 00:47:01 +0300

    fix flag to set instead of clear

--- Code/TransferData/TransferDataUtils.cs
+++ Code/TransferData/TransferDataUtils.cs
@@ -191,7 +191,7 @@ namespace TransferController
                         transfers[0].SpawnsVehicles = true;
 
                         // Prison Helicopter Mod.
-                        if ((buildingFlags & Building.Flags.Downgrading) == Building.Flags.None)
+                        if ((buildingFlags & Building.Flags.Downgrading) != 0)
                         {
                             transfers[1].PanelTitle = Translations.Translate("TFC_POL_PHI");
                             transfers[1].OutsideText = null;
@@ -249,7 +249,7 @@ namespace TransferController
                             if (buildingInfo.m_buildingAI.GetType().Name.Equals("PrisonCopterPoliceStationAI"))
                             {
                                 // Small (local) police station
-                                if ((buildingFlags & Building.Flags.Downgrading) != Building.Flags.None)
+                                if ((buildingFlags & Building.Flags.Downgrading) == 0)
                                 {
                                     // Send prisoners to big police station (collected by prison van sent from big station).
                                     transfers[2].PanelTitle = Translations.Translate("TFC_POL_PTO");
